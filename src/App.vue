64da3396967e43fc543d542e43b414a5e6a266a0
ketjj <jorjolianiket@gmail.com>
2022-05-31 14:51:39 +0200

    adding scroll to top button

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
+  <div v-cloak>
     <HeaderComp/>
     <MainComp/>
     <FooterComp/>
+
+
+    <div id="pagetop" v-show="scY > 300" @click="toTop"><i class="fa-solid fa-arrow-up"></i>
+    </div>
 
   </div>
 </template>
@@ -18,12 +22,61 @@ export default {
     HeaderComp,
     MainComp,
     FooterComp
-  }
+  },
+  data() {
+    return {
+      scTimer: 0,
+      scY: 0,
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods:{
+    handleScroll: function () {
+      if (this.scTimer) return;
+      this.scTimer = setTimeout(() => {
+      this.scY = window.scrollY;
+
+      clearTimeout(this.scTimer);
+      this.scTimer = 0;
+      }, 100);
+    },
+
+    toTop: function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    },
+ }
 }
 </script>
 
 <style lang="scss">
 
 @import './assets/style/general';
+@import './assets/style/vars';
+
+#pagetop{
+  position: fixed;
+  z-index: 995;
+  bottom: 40px;
+  right:50px;
+
+}
+
+.fa-arrow-up{
+  color:black;
+  padding: 12px 15px;
+  background-color: $oslo-gray;
+  border-radius: 50%;
+  box-shadow: 0 0 10px $chestNut-lighterred;
+  &:hover{
+    background-color:white;
+    cursor: pointer;
+    color:$chestNut-lighterred;
+  }
+}
 
 </style>
